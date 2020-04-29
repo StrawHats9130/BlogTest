6fc55a3eacb74364f81cf621653cb559a0b284f0
YaremaBen <yaremab@icloud.com>
2020-04-29 10:30:02 -0400

    added smtp
    Added the code for smtp not functional yet

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,11 @@
-﻿using BlogTest.Models;
+﻿using BlogTest.App_Start;
+using BlogTest.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -14,10 +18,10 @@ namespace BlogTest.Controllers
         public ActionResult Index()
         {
             //Get all the BlogPost that are Published
-            // return View(db.BlogPosts.Where(foo => foo.Published).OrderByDescending(b => b.Created).ToList());
+             return View(db.BlogPosts.Where(foo => foo.Published).OrderByDescending(b => b.Created).ToList());
 
             //temp string to test with
-            return View(db.BlogPosts.OrderByDescending(b => b.Created).ToList());
+            //return View(db.BlogPosts.OrderByDescending(b => b.Created).ToList());
             //return View(db.BlogPosts.ToList());
             //return View(db.BlogPosts.Where(b=>b.Published).ToList())
         }
@@ -31,9 +35,44 @@ namespace BlogTest.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            //ViewBag.Message = "Your contact page.";
+            EmailModel model = new EmailModel();
+            return View(model);
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                    var body = "<p>Email From: <bold>{0}</bold> ({1})</p><p>Message:</p><p>{2}</p>";
+                    var from = "MyPortfolio<example@email.com>";
+                    model.Body = "This is a message from your portfolio site.  The name and the email of the contacting person is above.";
+                    var email = new MailMessage(from, ConfigurationManager.AppSettings["emailto"])
+                    {
+                        Subject = "Portfolio Contact Email",
+                        Body = string.Format(body, model.FromName, model.FromEmail, model.Body),
+                        IsBodyHtml = true
+                    };
+
+                    var svc = new PersonalEmail();
+                    await svc.SendAsync(email);
+
+                    return View(new EmailModel());
+                }
+                catch (Exception ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                    await Task.FromResult(0);
+                }
+            }
+            return View(model);
+        }
+
+
     }
 }
